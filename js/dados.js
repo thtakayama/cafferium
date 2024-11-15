c9a92f9ad3dc55fb55e3b265bfd359ea341ab7f5
thtakayama <th.takayama58@gmail.com>
2024-11-14 22:36:59 -0300

    Adicionando itens no menu

--- js/dados.js
+++ js/dados.js
@@ -1,7 +1,147 @@
+//LANCHES
 const  saladaQuinoaLegumes = {
     titulo: 'Salada de Quinoa e Legumes',
     descricao: 'Quinoa, vegetais frescos e molho cítrico.',
     preco: '20,50',
     tags: ['Vegano', 'Sem Glúten'],
-    imagem: './img/menu/pao-de-queijo.jpg'
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
 };
+
+const  wrapAbacateGraoBico = {
+    titulo: 'Wrap de Abacate e Grão-de-Bico',
+    descricao: 'Tortilha de arroz com abacate e grão-de-bico.',
+    preco: '20,50',
+    tags: ['Vegano', 'Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
+};
+
+const  quicheEspinafreQueijo = {
+    titulo: 'Quiche de Espinafre e Queijo',
+    descricao: 'Quiche leve com espinafre e queijo cremoso.',
+    preco: '20,50',
+    tags: ['Vegetariano','Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
+};
+
+const  espagueteAbobrinha = {
+    titulo: 'Espaguete de Abobrinha com Molho Pesto',
+    descricao: 'Abobrinha em espiral com molho pesto e frango.',
+    preco: '20,50',
+    tags: ['Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
+};
+
+const  omeleteQueijoEspinafre = {
+    titulo: 'Omelete de Queijo e Espinafre',
+    descricao: 'Omelete leve com queijo e espinafre fresco.',
+    preco: '20,50',
+    tags: ['Vegetariano','Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
+};
+
+const  saladaCaesarFrango = {
+    titulo: 'Salada Caesar com Frango',
+    descricao: 'Frango grelhado, alface, croutons e molho Caesar.',
+    preco: '20,50',
+    tags: [],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
+};
+
+const  wrapPeitoPeru = {
+    titulo: 'Wrap de Peito de Peru',
+    descricao: 'Peito de peru, espinafre e cream cheese.',
+    preco: '20,50',
+    tags: [],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
+};
+
+const  sopaLentilha = {
+    titulo: 'Sopa de Lentilha',
+    descricao: 'Sopa reconfortante de lentilhas e temperos.',
+    preco: '20,50',
+    tags: ['Vegano', 'Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
+};
+
+const  torradaAbacate = {
+    titulo: 'Torrada de Abacate',
+    descricao: 'Pão sem glúten com abacate e tomate.',
+    preco: '20,50',
+    tags: ['Vegano', 'Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
+};
+
+const  torradaSalmaoDefumado = {
+    titulo: 'Torrada com Salmão Defumado',
+    descricao: 'Salmão defumado, cream cheese e cebola roxa.',
+    preco: '20,50',
+    tags: [],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'lanche',
+};
+
+//DOCES
+const  croissantChocolate = {
+    titulo: 'Croissant de Chocolate',
+    descricao: 'Croissant folhado recheado com chocolate amargo.',
+    preco: '20,50',
+    tags: [],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'doce',
+};
+
+const  pudimLeiteCondesado = {
+    titulo: 'Pudim de Leite Condensado',
+    descricao: 'Pudim cremoso com calda de caramelo.',
+    preco: '20,50',
+    tags: ['Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'doce',
+};
+
+const  tortaMaca = {
+    titulo: 'Torta de Maçã',
+    descricao: 'Torta clássica com maçãs caramelizadas e canela.',
+    preco: '20,50',
+    tags: ['Sem Lactose'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'doce',
+};
+
+const  queijadinha = {
+    titulo: 'Queijadinha',
+    descricao: 'Deliciosa tortinha de coco e queijo ralado.',
+    preco: '20,50',
+    tags: ['Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'doce',
+};
+
+const  boloChocolate = {
+    titulo: 'Bolo de Chocolate com Cobertura',
+    descricao: 'Bolo de chocolate fofinho com cobertura cremosa.',
+    preco: '20,50',
+    tags: ['Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'doce',
+};
+
+const  rocamboleDoceLeite = {
+    titulo: 'Rocambole de Doce de Leite',
+    descricao: 'Bolo enrolado com recheio de doce de leite.',
+    preco: '20,50',
+    tags: ['Sem Glúten'],
+    imagem: './img/menu/pao-de-queijo.jpg', 
+    tipo: 'doce',
+};
+
+//CAFES
